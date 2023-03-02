8f6968eb3d79dd14fa410d3b2c0ffaf5006aa85e
Truong Giang <giang.nt09845@sinhvien.hoasen.edu.vn>
2023-03-02 22:21:40 +0700

    update animation landingPage

--- client/src/app/page/landing-page/landing-page.module.ts
+++ client/src/app/page/landing-page/landing-page.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
-<<<<<<< HEAD
-import { SharedModule } from 'src/app/shared/shared/shared.module';
+
+import { SharedModule } from '../../shared/shared.module';
 import { NavbarLandingPageComponent } from './components/navbar-landing-page/navbar-landing-page.component';
-=======
-import { SharedModule } from 'src/app/shared/shared.module';
->>>>>>> 9613062335065d0567888f55d68f2681caeeb86e
+
 @NgModule({
   declarations: [
     LandingPageComponent,
