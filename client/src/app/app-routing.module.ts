bd3659fc2df2f465362577145973c31c890a22db
PhamIchChuyen220603 <chuyen.pi11316@sinhvien.hoasen.edu.vn>
2023-03-02 17:24:32 +0700

    pull

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./page/landing-page/landing-page.module').then(m => m.LandingPageModule) },
 { path: 'home', loadChildren: () => import('./page/home/home.module').then(m => m.HomeModule) },
-{ path: 'login-page', loadChildren: () => import('./page/login-page/login-page.module').then(m => m.LoginPageModule) },
+{ path: 'lpageogin-', loadChildren: () => import('./page/login-page/login-page.module').then(m => m.LoginPageModule) },
 { path: 'spreadsheet', loadChildren: () => import('./page/spreadsheet/spreadsheet.module').then(m => m.SpreadsheetModule) }];
 
 @NgModule({
