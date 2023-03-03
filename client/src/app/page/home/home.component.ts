962292e656ad39942d70453716354e7993cf000e
Quang Truong <truong.bq00118@sinhvien.hoasen.edu.vn>
2023-03-03 10:39:13 +0700

    mini change

--- client/src/app/page/home/home.component.ts
+++ client/src/app/page/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AuthService } from 'src/app/service/auth.service';
 import { FileService } from 'src/app/service/file.service';
+import { AuthState } from 'src/ngrx/states/auth.states';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,11 @@ import { FileService } from 'src/app/service/file.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor(public file: FileService) {}
+  constructor(
+    private fileService: FileService,
+    private authService: AuthService,
+    private store: Store<{ auth: AuthState }>
+  ) {}
 
-  file$ = this.file.fileArr;
+  file$ = this.fileService.getAllFiles();
 }
--- client/src/app/page/home/home.module.ts
+++ client/src/app/page/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TemplateComponent } from './components/template/template.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     NavbarComponent,
+    TemplateComponent,
 
   ],
   imports: [
