ba9aa857f0e97c502ed0c21d39a5b379322b8ca9
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2023-03-05 09:03:51 +0700

    catch id on param for spreadsheet

--- client/src/app/page/spreadsheet/spreadsheet.component.ts
+++ client/src/app/page/spreadsheet/spreadsheet.component.ts
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Spreadsheet, BeforeOpenEventArgs,
+import {
+  Spreadsheet, BeforeOpenEventArgs,
 } from '@syncfusion/ej2-angular-spreadsheet';
 import { getApp } from 'firebase/app';
 import { File } from 'src/app/model/file.model';
@@ -9,6 +10,9 @@ import { AuthService } from 'src/app/service/auth.service';
 import { FileService } from 'src/app/service/file.service';
 import { AuthState } from 'src/ngrx/states/auth.states';
 import { InvitationState } from 'src/ngrx/states/invitation.state';
+import { FileState } from 'src/ngrx/states/file.states';
+import { ActivatedRoute } from '@angular/router';
+import { FileActions } from 'src/ngrx/actions/file.actions';
 
 @Component({
   selector: 'app-spreadsheet',
@@ -18,25 +22,31 @@ import { InvitationState } from 'src/ngrx/states/invitation.state';
 })
 export class SpreadsheetComponent implements OnInit {
   @ViewChild('spreadsheet') spreadsheetObj!: Spreadsheet;
-
   hide() {
     this.spreadsheetObj.hideFileMenuItems(['File'], true);
   }
 
-  auth$!:Observable<AuthState>
-
+  auth$!: Observable<AuthState>
+  file$!: Observable<FileState>
   id!: string;
   constructor(
     private FileService: FileService,
     private authService: AuthService,
-    private store: Store<{ auth: AuthState, invite: InvitationState }>
+    private route: ActivatedRoute,
+    private store: Store<{ auth: AuthState, invite: InvitationState, file: FileState }>
   ) {
     this.auth$ = this.store.select('auth');
     this.auth$.subscribe((auth) => {
       this.id = auth.user?.userId || 'kh co user';
     });
   }
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // subscribe to url params
+    this.route.paramMap.subscribe(params => {
+      let filedId = params.get('id');
+      this.store.dispatch(FileActions.getFileById({ fileId: filedId! }));
+    });
+  }
 
   openUrl =
     'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open';
@@ -61,8 +71,8 @@ export class SpreadsheetComponent implements OnInit {
       data: response,
       status: "private",
       member: [],
-    }  
-    this.FileService.createFile(fileToUpLoad);    
+    }
+    this.FileService.createFile(fileToUpLoad);
   }
 
   async open(event: BeforeOpenEventArgs) {
