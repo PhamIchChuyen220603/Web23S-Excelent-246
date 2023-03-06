434380d4e6a3a5ebb08fafbbe17d27c983252c5f
nguyentantrung2000 <trung.nt4996@sinhvien.hoasen.edu.vn>
2023-03-06 16:02:04 +0700

    pull

--- client/src/app/page/spreadsheet/components/navbar/navbar.component.ts
+++ client/src/app/page/spreadsheet/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { ShareDialogComponent } from './../share-dialog/share-dialog.component';
 import { MenuItemModel } from './../../../../../../node_modules/@syncfusion/ej2-navigations/src/common/menu-base-model.d';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener, Input, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
 import { MenuEventArgs } from '@syncfusion/ej2-navigations';
@@ -9,21 +9,35 @@ import { MatDialog } from '@angular/material/dialog';
 import { OpenFileDialogComponent } from '../open-file-dialog/open-file-dialog.component';
 import { FileService } from 'src/app/service/file.service';
 
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  constructor(private router: Router, protected authService: AuthService, private dailog: MatDialog, protected fileService: FileService) {}
+  public title = 'Spreadsheet';
+  public isEditing: boolean;
+  public pendingValue: string;
+  public value!: 'Nguyen Tan Trung';
+  public valueChangeEvents: EventEmitter<string>;
+
+
+  constructor(private eRef: ElementRef, private router: Router, protected authService: AuthService, private dailog: MatDialog, protected fileService: FileService) {
+
+    this.isEditing = false;
+    this.pendingValue = "";
+    this.valueChangeEvents = new EventEmitter();
+  }
 
   @ViewChild('menu') menu!: ElementRef;
 
 
-  open(){
+
+  open() {
     this.dailog.open(OpenFileDialogComponent);
   }
-  share(){
+  share() {
     this.dailog.open(ShareDialogComponent);
   }
 
@@ -50,8 +64,8 @@ export class NavbarComponent {
     {
       text: 'File',
       items: [
-        { text: 'Open',},
-        { text: 'Save',},
+        { text: 'Open', },
+        { text: 'Save', },
         { text: 'Exit', },
 
       ],
@@ -59,9 +73,9 @@ export class NavbarComponent {
     {
       text: 'Edit',
       items: [
-        { text: 'Cut'},
-        { text: 'Copy'},
-        { text: 'Paste'},
+        { text: 'Cut' },
+        { text: 'Copy' },
+        { text: 'Paste' },
       ],
     },
     {
@@ -83,15 +97,41 @@ export class NavbarComponent {
   ];
 
 
-  openN(args: MenuEventArgs){
+  openN(args: MenuEventArgs) {
     let temp = this.menuItemText.find((item) => item == args.item.text);
-    if(temp == 'Open'){
+    if (temp == 'Open') {
       this.open();
     }
   }
+  public cancel(): void {
 
+    this.isEditing = false;
 
-  navigateToHome() {
-    this.router.navigate(['home']);
   }
+
+
+  // I enable the editing of the value.
+  public edit(): void {
+
+    this.pendingValue = this.value;
+    this.isEditing = true;
+
+  }
+
+
+  // I process changes to the pending value.
+  public processChanges(): void {
+
+    // If the value actually changed, emit the change but don't change the local
+    // value - we don't want to break unidirectional data-flow.
+    if (this.pendingValue !== this.value) {
+
+      this.valueChangeEvents.emit(this.pendingValue);
+
+    }
+
+    this.isEditing = false;
+
+  }
+
 }
